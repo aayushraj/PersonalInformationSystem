09be7357f514152e30b62b9cb75f077e7d99d768
Bidhya Dangol <bdangol21@gmail.com>
2016-07-21 16:42:32 +0545

    fifth=CourseinfoProvider

--- PersonalInformationSystem/Providers/CourseInfoProvider.cs
+++ PersonalInformationSystem/Providers/CourseInfoProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using PersonalInformationSystem.Models;
+using System.Data.Entity;
 
 namespace PersonalInformationSystem.Providers
 {
@@ -20,7 +21,30 @@ namespace PersonalInformationSystem.Providers
             entityCourseInfo.CourseAmount = model.CourseAmount;
             entityCourseInfo.Status = model.Status;
 
+            if(model.CourseId!=0 && model.CourseId > 0)
+            {
+                ent.Entry(entityCourseInfo).State = EntityState.Modified;
+                entityCourseInfo.ModifiedBy = 1;
+                entityCourseInfo.ModifiedOn = DateTime.Now;
+            }
+            else
+            {
+                ent.Entry(entityCourseInfo).State = EntityState.Added;
+                entityCourseInfo.CreatedBy = 1;
+                entityCourseInfo.CreatedOn = DateTime.Now;
+            }
+            ent.SaveChanges();
             return 1;
+        }
+        public bool Delete(int courseinfoId)
+        {
+            PersonalInformationSystemEntities ent = new PersonalInformationSystemEntities();
+            CourseInfo entityCourseInfo = ent.CourseInfoes.Where(x => x.CourseId == courseinfoId).FirstOrDefault();
+            entityCourseInfo.DeletedBy = 1;
+            entityCourseInfo.DeletedOn = DateTime.Now;
+            ent.Entry(entityCourseInfo).State = EntityState.Modified;
+            ent.SaveChanges();
+            return true;
         }
     }
 }
