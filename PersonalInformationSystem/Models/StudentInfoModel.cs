36b6a552ba58249662c9f929f52991bc3d9d5210
Aayush Rajopadhyaya <aayushrajopadhyaya@gmail.com>
2016-07-21 14:44:53 +0545

    crete view and student info for multiple view in a model

--- PersonalInformationSystem/Models/StudentInfoModel.cs
+++ PersonalInformationSystem/Models/StudentInfoModel.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using PersonalInformationSystem.Models;
 
 namespace PersonalInformationSystem.Models
 {
     public class StudentInfoModel
     {
         public int StudentId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        [Required]
         public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
         public string Address { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
         public int Gender { get; set; }
         public string Email { get; set; }
         public string MobileNo { get; set; }
+        public string ResidenceNo { get; set; }
         public DateTime EnrolledDate { get; set; }
         public bool ComputerLiterate { get; set; }
         public int CreatedBy { get; set; }
@@ -26,5 +34,8 @@ namespace PersonalInformationSystem.Models
         public DateTime DeletedOn { get; set; }
         public bool Status { get; set; }
         public List<StudentInfoModel> StudentInfoModelList { get; set; }
+        public CourseInfoModel ObjCourseInfoModel { get; set; }
+        public PaymentModel ObjPaymentModel { get; set; }
+
     }
 }
---
+++ PersonalInformationSystem/Utility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PersonalInformationSystem
+{
+    public class Utility
+    {
+        public static IEnumerable<SelectListItem> GetGender()
+        {
+            return new SelectList(new[]
+            {
+
+                new {Id="1",Value="Male"},
+                new {Id="2",Value="Female"},
+                
+
+
+            }, "value", "Value");
+        }
+    }
+}
